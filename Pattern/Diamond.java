1581859cc2b40308b27ae5a7ecc6b0b82cbabe8b
sandeshsolanki <sandeshsolanki4@gmail.com>
2025-09-09 21:09:52 +0530

    add a new program of pattern of diamond

---
+++ Pattern/Diamond.java
@@ -0,0 +1,37 @@
+package Pattern;
+
+public class Diamond {
+    public static void diamond(int n) {
+        //for loop for line
+        for (int line = 1; line <= n; line++) {
+            // for loop for space
+            for (int space = 1; space <= n - line; space++) {
+                System.out.print(" ");
+            }
+            // for loop for star
+            for (int star = 1; star <= (2 * line - 1); star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int line = n; line >= 1; line--) {
+            // for loop for space
+            for (int space = 1; space <= n - line; space++) {
+                System.out.print(" ");
+            }
+            // for loop for star
+            for (int star = 1; star <= (2 * line - 1); star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        diamond(5);
+    }
+}
